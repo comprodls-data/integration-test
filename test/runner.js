5af0698a20a59ecec331ee45266f9ea1ed03c5c7
manishcompro <manish.mehta@comprotechnologies.com>
2019-01-11 18:38:05 +0530

    updated script for posting metrics to statuspage

--- test/runner.js
+++ test/runner.js
@@ -6,6 +6,7 @@ var statusPageUrl = 'https://api.statuspage.io/v1';
 var page_id = 'tzx1szydvwcf';
 var component_id = 'vq1z6f84vf4w';
 var metric_id = 'b8k2xjt3b0kv';
+var teacher_registraction_metric_id = '1hbc9r4pn96w'
 var api_key = 'c3cb0449-e3b7-4618-a938-bcae06256d82'
 
 describe('Status Page', function () {
@@ -19,9 +20,17 @@ describe('Status Page', function () {
 
   var reqBody1 = { 
   "metric": {
-    "name": 'Student Registration1'
+    "name": 'Student Registration2'
   }
 };
+
+  var reqBody2 = {
+"data": {
+    "timestamp": 1547211087,
+     "value": 120
+}};
+
+
   
   it('Update Teacher Registration', function (done) {
       request
@@ -41,6 +50,21 @@ describe('Status Page', function () {
       request
         .patch(statusPageUrl + '/pages/' + page_id + '/metrics/'  + metric_id)
         .send (reqBody1)
+		.set({'content-type' : 'application/json'})
+        .set({'Authorization' : 'c3cb0449-e3b7-4618-a938-bcae06256d82'})
+        .end(function (err, res) {
+          if (err) { done(err); }
+          else {
+            console.log("response\n" + JSON.stringify(res.body, null, 1));
+            done();
+          }
+        });
+  });
+  
+      it('Update teacher registraction Metrics', function (done) {
+      request
+        .post(statusPageUrl + '/pages/' + page_id + '/metrics/' + teacher_registraction_metric_id + '/data.json')
+        .send (reqBody2)
 		.set({'content-type' : 'application/json'})
         .set({'Authorization' : 'c3cb0449-e3b7-4618-a938-bcae06256d82'})
         .end(function (err, res) {
